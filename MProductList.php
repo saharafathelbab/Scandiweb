89ee2b18829e68db862a6705c09bda2009e9839b
saharafathelbab <33489137+saharafathelbab@users.noreply.github.com>
2022-05-09 15:44:23 -0400

    Adding MProductList.php

---
+++ MProductList.php
@@ -0,0 +1,18 @@
+<?php 
+
+abstract class MProductList
+{
+    private $sku;
+    private $name;
+    private $price;
+    private $size;
+    private $height;
+    private $width;
+    private $length;
+    private $weight;
+    
+    abstract public function setValues($sku, $name, $price, $size, $height, $width, $length, $weight);
+    abstract public function getInfo();
+}
+
+?>
